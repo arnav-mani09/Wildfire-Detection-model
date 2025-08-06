15fd69c7a400cf60423939c40bc2a7dfed5466d8
arnav-mani09 <145302787+arnav-mani09@users.noreply.github.com>
2025-08-05 18:14:48 -0700

    Update to YOLOv13 model

--- train_yolo.py
+++ train_yolo.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load the YOLOv13 small model (you can try "yolov13m.pt" or "yolov13n.pt" later)
-model = YOLO("yolov8s.pt")
+model = YOLO("weights/yolov13s.pt")
 
 # Train the model (make sure you have a data.yaml file in your project folder)
 model.train(
